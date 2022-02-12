313133f2f10803f06283ac83e46227c9663b3f3d
Alexis-Elaxis <52383750+Alexis-Elaxis@users.noreply.github.com>
2022-02-12 17:26:52 +0100

    main

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-require('dotenv').config();
-const {TwitterClient} = require('twitter-api-client');
-
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
+const config = require('./config');
+const twitter = require('twitter-lite');
+const client = new twitter(config);
