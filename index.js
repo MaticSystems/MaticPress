d2ff3dfc490ea1cf11e133e1d0d053d7f8395638
Alexis-Elaxis <52383750+Alexis-Elaxis@users.noreply.github.com>
2022-02-12 17:17:02 +0100

    Changement du package

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const {TwitterClient} = require('twitter-api-client');
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
